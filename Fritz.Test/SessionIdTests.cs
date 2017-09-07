98406dd2a8914bd1b1f3aa95e715a70a417e7574
Christian Storb <christian.storb@outlook.com>
2017-09-07 22:01:08 +0200

    SessionIdTests added

---
+++ Fritz.Test/SessionIdTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+using System.IO;
+
+namespace FritzTR064.Test
+{
+    /// <summary>
+    /// Beispiel-Code für den Bezug einer Session-ID ab FRITZ!OS 5.50 
+    /// </summary>
+    [TestClass]
+    public class SessionIdTests
+    {
+        private string UserName { get; set; }
+        private string Password { get; set; }
+        private string Url { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            UserName = Environment.GetEnvironmentVariable("FritzBoxUserName");
+            Password = Environment.GetEnvironmentVariable("FritzBoxPassword");
+
+            Url = $"http://fritz.box:{49000}";
+
+            //Url = $"https://fritz.box:{this.SecurityPort}";
+        }
+
+        [TestMethod]
+        public void TestSessionId()
+        {
+            // SessionID ermitteln  
+            string sid = GetSessionId(UserName, Password);
+            string seite = SeiteEinlesen(@"http://fritz.box/home/home.lua", sid);
+            Console.WriteLine(seite);
+        }
+
+        public string SeiteEinlesen(string url, string sid)
+        {
+            Uri uri = new Uri(url + "?sid=" + sid);
+            HttpWebRequest request = WebRequest.Create(uri) as HttpWebRequest;
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string str = reader.ReadToEnd();
+            return str;
+        }
+
+        public string GetSessionId(string benutzername, string kennwort)
+        {
+            XDocument doc = XDocument.Load(@"http://fritz.box/login_sid.lua");
+            string sid = GetValue(doc, "SID");
+            if (sid == "0000000000000000")
+            {
+                string challenge = GetValue(doc, "Challenge");
+                string uri = @"http://fritz.box/login_sid.lua?username=" + benutzername + @"&response=" + GetResponse(challenge, kennwort);
+                doc = XDocument.Load(uri); sid = GetValue(doc, "SID");
+            }
+            return sid;
+        }
+
+        public string GetResponse(string challenge, string kennwort)
+        {
+            return challenge + "-" + GetMD5Hash(challenge + "-" + kennwort);
+        }
+
+        public string GetMD5Hash(string input)
+        {
+            MD5 md5Hasher = MD5.Create();
+            byte[] data = md5Hasher.ComputeHash(Encoding.Unicode.GetBytes(input));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sb.Append(data[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+
+        public string GetValue(XDocument doc, string name)
+        {
+            XElement info = doc.FirstNode as XElement;
+            return info.Element(name).Value;
+        }
+    }
+}
--- Fritz.Test/UnitTest2.cs
+++ Fritz.Test/UnitTest2.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Fritz;
 using System.Net;
@@ -98,46 +99,77 @@ namespace Fritz.Test
 
             ushort onTelNumberOfEntries;
             service.GetNumberOfEntries(out onTelNumberOfEntries);
+            
+            ushort phonebookId = 0;
+            string phonebookName;
+            string phonebookExtraID;
+            string phonebookURL;
+            service.GetPhonebook(phonebookId, out phonebookName, out phonebookExtraID, out phonebookURL);
+
+            var client = new WebClient();
+            client.DownloadFile(phonebookURL, "pbook.xml");
 
             string phonebookList;
             service.GetPhonebookList(out phonebookList);
 
-            ushort phonebookId = 0;
-            string phonebookName;
-            string phonebookExtraID;
-            string PhonebookURL;
-            service.GetPhonebook(phonebookId, out phonebookName, out phonebookExtraID, out PhonebookURL);
+            string callListUrl;
+            service.GetCallList(out callListUrl);
+            client.DownloadFile(callListUrl, "calllist.xml");
 
             uint phonebookEntryID = 0;
             string phonebookEntryData;
             service.GetPhonebookEntry(phonebookId, phonebookEntryID, out phonebookEntryData);
+        }
+
+        [TestMethod]
+        public void TestSetPhonebookEntry()
+        {
+            var service = new Contact(Url);
+            service.SoapHttpClientProtocol.Credentials = new NetworkCredential(userName: UserName, password: Password);
+
+            ushort phonebookId = 0;
 
             // Add new entries with "" as value for PhonebookEntryID.Change existing entries with a value used for 
             // PhonebookEntryID with GetPhonebookEntry.The variable PhonebookEntryData may contain a unique ID.
-            phonebookEntryID = 0;
-            phonebookEntryData = "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+            uint phonebookEntryID = 0;
+            string phonebookEntryData = "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
                 + "<contact>"
                 + "<category/>"
                 + "<person>"
-                + "<realName>Mustermann, Marianne</realName>" 
+                + "<realName>Mustermann, Marianne</realName>"
                 + "</person>"
-                + "<telephony nid=\"1\">" 
+                + "<telephony nid=\"1\">"
                 + "<number type=\"home\" id=\"0\" vanity=\"\" prio=\"1\">0123456789</number>"
                 + "</telephony>"
                 + "<services/>"
                 + "<setup>"
-                + "<ringTone/>" 
+                + "<ringTone/>"
                 + "</setup>"
                 + "<mod_time>1487852657</mod_time>"
-                + "<uniqueid></uniqueid>" 
+                + "<uniqueid></uniqueid>"
                 + "</contact>";
 
             service.SetPhonebookEntry(phonebookId, phonebookEntryID, phonebookEntryData);
-
-            string callListUrl;
-            service.GetCallList(out callListUrl);
         }
 
+        [TestMethod]
+        public void TestGetDectHandsetList()
+        {
+            var service = new Contact(Url);
+            service.SoapHttpClientProtocol.Credentials = new NetworkCredential(userName: UserName, password: Password);
+
+            string dectIDList;
+            service.GetDECTHandsetList(out dectIDList);
+
+            var dectIds = dectIDList.Split(',');
+            foreach(var id in dectIds)
+            { 
+                string handsetName;
+                ushort phonebookID;
+                service.GetDECTHandsetInfo((ushort)Convert.ToInt32(id), out handsetName, out phonebookID);
+            }
+        }
+        
         [TestMethod]
         public void TestDeviceConfigRebootFritzBox()
         {

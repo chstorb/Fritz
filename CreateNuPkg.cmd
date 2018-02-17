e894d29a3c8e6ac96bc393afc63f48993fef71f0
Christian Storb <christian.storb@outlook.com>
2018-02-17 22:45:29 +0100

    nuget package update

---
+++ CreateNuPkg.cmd
@@ -0,0 +1,3 @@
+# When using a Visual Studio project, run nuget pack with your project file, which automatically loads 
+# the project's .nuspec file and replaces any tokens within it using values in the project file.
+nuget pack
---
+++ CreateNuSpec.cmd
@@ -0,0 +1,2 @@
+# Use in a folder containing a project file <project-name>.csproj or <project-name>.vbproj
+nuget spec
